457af6e53df332a5dc385614def7f224e42cd5e6
siaps-varjota <previnevarjota@gmail.com>
2026-04-20 12:38:18 -0300

    Update ResultadoFinalTab.tsx

--- src/components/dashboard/ResultadoFinalTab.tsx
+++ src/components/dashboard/ResultadoFinalTab.tsx
@@ -1052,7 +1052,7 @@ y += cardH + 14;
           },
         });
 
-        y = (doc as any).lastAutoTable.finalY + 10;
+        y = (doc as any).lastAutoTable.finalY + 11;
       }
 
       doc.save(`resultado-final-${new Date().toISOString().split("T")[0]}.pdf`);
