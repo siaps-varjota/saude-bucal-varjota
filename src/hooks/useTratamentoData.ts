1d2d5eee10f25b1eead3addf8baf34c948d34a8d
siaps-varjota <previnevarjota@gmail.com>
2026-05-22 21:19:34 -0300

    Update useTratamentoData.ts

--- src/hooks/useTratamentoData.ts
+++ src/hooks/useTratamentoData.ts
@@ -56,7 +56,7 @@ const parseCSV = (csv: string): TratamentoPatient[] => {
         idade,
         primeiraConsulta: fields[8] || "-",
         tratamentoConcluido: fields[9] || "-",
-        comTratamentoConcluido: fields[10]?.trim() || "Sem 1ª Consulta",
+        comTratamentoConcluido: fields[10] || "NÃO",
       };
     })
     .filter((patient) => patient.id > 0);
